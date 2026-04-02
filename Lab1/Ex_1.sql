ee77a653cbd6a9721e6e6d5ee23f66bf8c122bdf
Davi Quirino <160233282+daviiq@users.noreply.github.com>
2026-04-02 01:18:44 +0000

    Inicio dos exercicios de SQL

---
+++ Lab1/Ex_1.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS lab1;
+CREATE DATABASE lab1;
+USE lab1;
+
+ CREATE TABLE Departamento (
+ idDepartamento int(11) NOT NULL,
+ dNome varchar(255) NOT NULL,
+ Orcamento decimal(10,0) NOT NULL,
+ PRIMARY KEY (idDepartamento)
+ );
+
+CREATE TABLE Funcionario (
+ idFuncionario INT PRIMARY KEY AUTO_INCREMENT,
+ Nome VARCHAR(45) NOT NULL,
+ Sobrenome VARCHAR(45) NOT NULL,
+ idDepartamento INT NOT NULL,
+ CONSTRAINT fk_Funcionario_Departamento FOREIGN KEY (idDepartamento)
+ REFERENCES Departamento (idDepartamento)
+ );
+
+INSERT INTO Departamento(idDepartamento,dNome,Orcamento) VALUES
+(5,'TI',5000),
+(6,'TI',2000);
+ INSERT INTO Funcionario(nome,Sobrenome,idDepartamento) VALUES
+ ('Davi', 'Israel',5),
+ ('Pedro','Israel',6);
+ 
+ 
+ SELECT Sobrenome FROM Funcionario
+ 
